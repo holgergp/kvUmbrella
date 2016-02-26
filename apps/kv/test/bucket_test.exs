7b8d306ea0cda3f0c4ea92049f848aea095a24da
Holger Grosse-Plankermann <hgp@Holgers-MacBook-Pro-2.local>
2016-02-26 15:53:53 +0100

    fixing remotes

---
+++ apps/kv/test/bucket_test.exs
@@ -0,0 +1,31 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+      {:ok, bucket} = KV.Bucket.start_link
+      {:ok, bucket: bucket, blub: "blah"}
+  end
+
+
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "deletes value by key", context do
+    %{bucket: bucket} = context
+    %{blub: foo} = context
+
+    assert foo == "blah"
+
+
+
+    assert KV.Bucket.get(bucket, "milk") == nil
+    KV.Bucket.put(bucket, "milk", 3)
+
+    KV.Bucket.delete(bucket, "milk")
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
+end
